bd480de8f6165e12d930fcd951ec065a39eef5a8
Amit Goldenberg <amit.wifi@gmail.com>
2019-10-24 19:23:07 +0300

    hello.py get newest csv, new feed dynamic display type

--- templates/new-feed/feed.js
+++ templates/new-feed/feed.js
@@ -2,38 +2,66 @@ google.charts.load('current', { 'packages': ['gauge'] });
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
-    const options = getGaugesOptions();
+    let dispType = {};
+    const options = getGaugesOptions(dispType);
     let gauges = new Array();
+    let texts = new Array();
     let data = getLatestBeacon();
 
     for (let i = 0; i < options.length; i++) {
-        let gauge = document.createElement(data[i][1] + "-gauge");
+        if (dispType[data[i][0]] === "gauge") {
+            let gauge = document.createElement("div");
+            document.body.appendChild(gauge);
+            gauges.push(new google.visualization.Gauge(gauge));
+        }
+        else if (dispType[data[i][0]] === "textbox") {
+            let textbox = document.createElement("textbox");
+            document.body.appendChild(textbox);
+            texts.push(textbox);
+        }
 
-        document.body.appendChild(gauge);
-        gauges.push(new google.visualization.Gauge(gauge));
     }
 
-    drawGauges(gauges, options, data);
+    drawCharts(gauges, options, data, dispType);
 
     setInterval(function () {
         data = getLatestBeacon();
-        drawGauges(gauges, options, data);
+        drawCharts(gauges, options, data, dispType);
     }, 1000);
 }
 
-function drawGauges(chart, options, data) {
+function drawCharts(charts, options, data, dispType) {
     for (let i = 0; i < options.length; i++) {
         const option = options[i];
-        const gaugeData = google.arrayToDataTable(data[i])
-        chart[i].draw(gaugeData, option);
+        if (dispType[data[i][0]] === "gauge") {
+            const gaugeData = google.arrayToDataTable(data[i])
+            charts[i].draw(gaugeData, option);
+        }
+        else if (dispType[data[i][0]] === "textbox") {
+            const data = data[i][1];
+            charts[i].innerHTML = data;
+        }
     }
 }
 
 function getLatestBeacon() {
+    let data;
+    $.ajax({
+        type: "POST",
+        url: "/feed",
+        data: {}
+    }).done(function (params) {
+        beacon = params;
+    });
 
+    let beacon = [];
+    for (let index = 0; index < data.length; index++) {
+        beacon.push(data[index]);
+    }
+    return beacon;
 }
 
-function getGaugesOptions() {
+function getGaugesOptions(dispType) {
     let optionData = new Array();
     //DOTO: Implement getting options
 
@@ -41,11 +69,16 @@ function getGaugesOptions() {
 
     for (let i = 0; i < optionsData.length; i++) {
         const optionData = optionsData[i];
-        options.push({
-            width: 400, height: 120,
-            redFrom: optionData[0], redTo: optionData[1],
-            minorTicks: 5
-        });
+        if (dispType[optionData[0]] === "gauge") {
+            options.push({
+                width: 400, height: 120,
+                redFrom: optionData[1], redTo: optionData[2],
+                minorTicks: 5
+            });
+        }
+        else if (dispType[optionData[0]] === "textbox") {
+            options.push(optionData[1]);
+        }
     }
 
     return options;
