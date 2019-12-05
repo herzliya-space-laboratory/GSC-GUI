6f23235ffe99a0424ac27c6982286a03cf94184d
Amit Goldenberg <amit.wifi@gmail.com>
2019-12-05 19:05:51 +0200

    Add checking if dump name exists in dump search

--- static/js/side_bar.js
+++ static/js/side_bar.js
@@ -7,6 +7,11 @@ function navChange(btn) {
 
 function searchDump(inputId) {
     let searchInputValue = $("#" + inputId).val();
+    if (dumpDict[searchInputValue] == undefined) {
+
+        alert(`Dump \"${searchInputValue}\" not found`)
+        return;
+    }
     let st = dumpDict[searchInputValue]["st"];
     let sst = dumpDict[searchInputValue]["sst"];
     let winLocation = window.location;
